151898a156eee404abb0d1f75a0d4aa42f25317d
Priyansh Ankre <priyansh.ankre@magicedtech.com>
2022-09-22 23:09:09 +0530

    added code splitting with routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from "./components/Home";
-import Cart from "./components/Cart";
+// import Home from "./components/Home";
+// import Cart from "./components/Cart";
+import { lazy } from "react";
+import { Suspense } from "react";
+
+const Home = lazy(()=>import('./components/Home'))
+const Cart = lazy(()=>import('./components/Cart'))
+const Header = lazy(()=>import('./components/Header'))
 
 function App() {
   return (
     <BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
       <Header />
       <div className="App">
         <Route path="/" exact>
@@ -16,6 +23,7 @@ function App() {
           <Cart />
         </Route>
       </div>
+      </Suspense>
     </BrowserRouter>
   );
 }
